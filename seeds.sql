3f8d90930ba3fa0a5918370f745f03c294b9d1c1
learnesh <32178001+learnesh@users.noreply.github.com>
2018-03-15 23:41:17 -0500

    Create seeds.sql

---
+++ seeds.sql
@@ -0,0 +1,7 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name, devoured) 
+VALUES 
+('Double Bacon Cheeseburger', 0),
+('Black Bean Burger', 0),
+('Big Mac Burger', 0),
+('Heart Attack Burger', 1);
